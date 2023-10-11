929bebef8a99ff517e29c6f33f07976fc45dc8e6
Paul K <paulmkouadio@gmail.com>
2023-10-11 10:11:34 +0000

    feat: first commit featuring basic setup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useFonts, Inter_900Black} from "@expo-google-fonts/inter"
 
 export default function App() {
+  const [ fontsLoaded, fontError ] = useFonts({
+    Inter_900Black
+  });
+
+  if(!fontsLoaded && !fontError){
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontFamily: "Inter_900Black", fontSize: 24}}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ README.md
@@ -0,0 +1,8 @@
+# Expo Quickstart
+I grew rather tired of having to go through the exact same steps each time I wanted to set up an Expo project, so I'm making a quickstart repo.
+
+It includes:
+- Expo Router
+- TypeScript
+- Google Fonts integration (currently imports Inter)
+- SVG support (`import SampleSVG from "./sample.svg"`)
